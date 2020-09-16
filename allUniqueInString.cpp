5d3c37ec0b9e1dcb7888fec4a8fe7b47b68320d9
pkmisra <pkmisra1999@gmail.com>
2020-09-16 12:04:57 +0530

    isUnique added

---
+++ allUniqueInString.cpp
@@ -0,0 +1,35 @@
+/* Check if string has all unique characters */
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//TODO:comoplexity == O(n) ; space => O(1)
+
+bool isUnique(string str){
+    if(str.length() > 128) return false;
+    //create array of boolean
+
+    vector<bool> char_set(128);
+
+//traverse and check that if the char at char_set is true or false...if it found then return false 
+    for(int i =0; i<str.length(); i++){
+        int val = str[i] - 'a';
+        if(char_set[val]){
+            return false;
+        }
+        char_set[val] = true;
+    }
+    return true;
+}
+
+
+int main(){
+    string str = "bcdaf";
+
+    if(isUnique(str) == true) cout<<"All unique \n";
+    else cout<<"NOt Unique";
+
+    return 0;
+}
